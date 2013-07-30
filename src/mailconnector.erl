0b9dc5e36ca603212e9f4932b4b279dedac3d2fd
Thomas Kobber Panum <thomas@panum.dk>
2013-07-30 21:53:59 +0200

    Finally resolved the error

--- src/mailconnector.erl
+++ src/mailconnector.erl
@@ -6,7 +6,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3, search_prefix/2]).
 
 start_link(Host, imap) ->
-    gen_server:start_link({?MODULE, local}, ?MODULE, [Host, 143], []).
+    gen_server:start_link({global, Host}, ?MODULE, [Host, 143], []).
 
 stop() ->
 	gen_server:call(?MODULE, stop).
